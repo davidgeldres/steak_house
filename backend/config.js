6698ab220aa8ddd35e9c4e842f61c70099a9ba3f
David Geldres <davidgeldres777@gmail.com>
2025-12-11 03:20:26 -0500

    Fix config env for Railway

--- backend/config.js
+++ backend/config.js
@@ -1,18 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-    db: {
-        host: process.env.DB_HOST || "localhost",
-        user: process.env.DB_USER || "root",
-        password: process.env.DB_PASSWORD || "Admin123!",
-        database: process.env.DB_NAME || "roca_steak",
-        port: process.env.DB_PORT || 3306,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0,
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET || "secreto_super_seguro",
-        expiresIn: "8h",
-    },
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  }
 };
